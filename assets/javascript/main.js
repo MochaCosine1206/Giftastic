909edb2c421b3ded7f0d7513e745f3b1dedaf7bc
StevenXPS13 <steven.parrott@gmail.com>
2018-12-12 12:02:36 -0700

    fully functioning minus tag saving

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -68,7 +68,7 @@ function callGifs() {
             }
             buttonColor();
             var gifDownLoad = $("<a>").addClass("btn-floating halfway-fab waves-effect waves-light blue").html('<i class="material-icons">file_download</i>').attr("id", "downButton").attr("gifID", response.data[i].id).attr("favState", "no");
-            gifFav.on("click", function(){
+            gifFav.on("click", function () {
                 var favState = $(this).attr("favState");
                 console.log(favState)
                 if (favState === "no") {
@@ -88,17 +88,17 @@ function callGifs() {
                     $("#favCount").html(favCount);
                 }
             })
-            
+
             var gifRating = $("<span>").addClass("card-title").html("<p class='flow-text'>Rating: " + response.data[i].rating + '</p>');
             var gifDesc = $("<div>").addClass("card-content white").text(response.data[i].title);
             $("#gifCont").prepend(gifCard);
             gifPicDiv.append(gifPic);
             $("#gifCard" + [i]).prepend(gifPicDiv, gifRating, gifDesc, gifFav, gifDownLoad);
-            
+
         }
         // below on click is to change state of div to favorite and push it to an object.   
-        
-        
+
+
     })
 }
 
@@ -111,23 +111,70 @@ function callFavGifs() {
     if (!Array.isArray(favStore)) {
         favStore = [];
         console.log(favStore);
-      }
+    }
 
-      favURL = "http://api.giphy.com/v1/gifs?api_key=fOl7fKImZ3vPLJkKlPB9WHGkhIV488wM&ids=" + favKeyChain;
+    favURL = "http://api.giphy.com/v1/gifs?api_key=fOl7fKImZ3vPLJkKlPB9WHGkhIV488wM&ids=" + favKeyChain;
 
-      $.ajax({
+    $.ajax({
         url: favURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
+        $("#favCard").empty()
+        for (var i = 0; i < favStore.length; i++) {
+            var gifCard = $("<div>").addClass("card " + fullColorGamut[i + colorCounter] + " z-depth-3 col s12 m3").attr("id", "gifCard" + [i]);
+            var gifPicDiv = $("<div>").addClass("card-img")
+            var gifPic = $("<img>").attr("src", response.data[i].images.fixed_height.url).attr("data-still", response.data[i].images.fixed_height_small_still.url).attr("data-animate", response.data[i].images.fixed_height_small.url).attr("data-state", "still");
+            gifFav = $("<a>").addClass("btn-floating halfway-fab waves-effect waves-light green").html('<i class="material-icons">stars</i>').attr("id", "favButton").attr("gifID", response.data[i].id).attr("favState", "yes");
+            var gifDownLoad = $("<a>").addClass("btn-floating halfway-fab waves-effect waves-light blue").html('<i class="material-icons">file_download</i>').attr("id", "downButton").attr("gifID", response.data[i].id).attr("favState", "no");
 
-      })
-      
-      
+            gifFav.on("click", function () {
+                var favState = $(this).attr("favState");
+                console.log(favState)
+                if (favState === "no") {
+                    $(this).attr("class", "btn-floating halfway-fab waves-effect waves-light green");
+                    $(this).attr("favState", "yes");
+                    favStore.push($(this).attr("gifID"));
+                    localStorage.setItem("gifKey", JSON.stringify(favStore));
+                    favCount = favStore.length;
+                    $("#favCount").html(favCount);
+                } else {
+                    $(this).attr("class", "btn-floating halfway-fab waves-effect waves-light red");
+                    $(this).attr("favState", "no");
+                    var removeFav = $(this).attr("gifID");
+                    favStore.splice(removeFav, 1);
+                    localStorage.setItem("gifKey", JSON.stringify(favStore));
+                    favCount = favStore.length;
+                    $("#favCount").html(favCount);
+                    $(this).parent().remove()
+                    favEmpty();
+                }
+            })
+
+            var gifRating = $("<span>").addClass("card-title").html("<p class='flow-text'>Rating: " + response.data[i].rating + '</p>');
+            var gifDesc = $("<div>").addClass("card-content white").text(response.data[i].title);
+            $("#favCard").append(gifCard);
+            gifPicDiv.append(gifPic);
+            $("#gifCard" + [i]).prepend(gifPicDiv, gifRating, gifDesc, gifFav, gifDownLoad);
+        }
+    })
+
+
+
 }
 
 
-
+function favEmpty() {
+    if (favStore.length === 0) {
+        console.log("yes");
+        var emptyMessage = $("<div>");
+        emptyMessage.attr("id", "emptyId");
+        emptyMessage.text("It's all Gone!!!<br>").html("<a href='index.html'>Head Back to the Home Page for More Great GIFS!!!</a>")
+        $("#favCard").append(emptyMessage);
+    } else {
+        console.log("no");
+    }
+}
 
 function renderButton() {
     $("#button-area").empty();
@@ -141,23 +188,25 @@ function renderButton() {
         button.append('<i class="close material-icons">close</i>');
         $("#button-area").append(button);
 
+
     }
 }
 
 
 
-$(document).ready(function(){
+$(document).ready(function () {
     callFavGifs();
+    favEmpty();
     $("#favCount").html(favCount);
     renderButton();
-    $(document).on("click", "#topicButton",  function(){
+    $(document).on("click", "#topicButton", function () {
         clickCounter++;
         console.log(clickCounter);
         offset = 0;
         tagClick = $(this).attr("data-name");
         console.log($(this))
         $(this).off("click");
-        callGifs();  
+        callGifs();
     });
     $(document).on("click", "#clickMore", function () {
         offset += 10;
@@ -165,7 +214,7 @@ $(document).ready(function(){
         callGifs();
     })
 
-    
+
     // $("body").css("background-image", "linear-gradient(to bottom right, " + color0 + ", " + color9);
     $("#add-tag").on("click", function (event) {
         event.preventDefault();
@@ -175,10 +224,10 @@ $(document).ready(function(){
         renderButton();
     });
 
-   
-    
-    
-    
+
+
+
+
 });
 
 
